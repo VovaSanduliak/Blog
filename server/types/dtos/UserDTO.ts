95f8d49a3f35e6133884e363ccb058783f03ef94
Vova Sanduliak <vovasanduliak@gmail.com>
2024-05-08 11:58:18 +0300

    changed user dto constructor

--- server/types/dtos/UserDTO.ts
+++ server/types/dtos/UserDTO.ts
@@ -8,8 +8,9 @@ class UserDto {
   role: UserRole;
   isActivated: boolean;
 
-  constructor(user: IUser & { _id: ObjectId }) {
-    this.id = user._id.toString();
+  constructor(user: IUser, _id: ObjectId) {
+
+    this.id = _id.toString();
     this.email = user.email;
     this.role = user.role;
     this.isActivated = user.isActivated || false;
