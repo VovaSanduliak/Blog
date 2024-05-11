8e7d591eba33245bf3374fbee1cba2dc3ab748e1
VovaSanduliak <vovasanduliak@gmail.com>
2024-05-11 18:02:38 +0300

    added nickname field to user model

--- server/models/user-model.ts
+++ server/models/user-model.ts
@@ -3,6 +3,7 @@ import UserRole from "../types/enums/UserRole";
 import IUser from "../types/interfaces/IUser";
 
 const userSchema = new mongoose.Schema<IUser>({
+  nickname: { type: String, required: true },
   email: {
     type: String,
     required: true,
--- server/types/interfaces/IUser.ts
+++ server/types/interfaces/IUser.ts
@@ -2,6 +2,7 @@ import mongoose, { ObjectId } from "mongoose";
 import UserRole from "../enums/UserRole";
 
 interface IUser {
+  nickname: string;
   email: string;
   role: UserRole;
   password: string;
